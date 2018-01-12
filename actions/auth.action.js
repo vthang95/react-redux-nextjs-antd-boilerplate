bd85184d3fb442cc5f9ae7c84813006e3ea24363
vthang95 <tranvietthang95@gmail.com>
2018-01-12 09:55:44 +0700

    Change Action name

--- actions/auth.action.js
+++ actions/auth.action.js
@@ -3,20 +3,20 @@ import { find } from 'lodash'
 
 export const authGetInfo = info => {
   return {
-    type: 'AUTH_GET_INFO',
+    type: 'AUTH::GET_INFO',
     payload: info
   }
 }
 
 export const authControlLoginModal = value => {
   return {
-    type: 'AUTH_CONTROL_LOGIN_MODAL',
+    type: 'AUTH::CONTROL_LOGIN_MODAL',
     payload: value
   }
 }
 
 export const authLogin = info => {
   const matchUser = find(data.accounts, user => user.username === info.username && user.password === info.password)
-  if (matchUser) return { type: 'AUTH_LOGIN_SUCCESS', payload: info }
-  return { type: 'AUTH_LOGIN_FAILED', payload: null }
+  if (matchUser) return { type: 'AUTH::LOGIN_SUCCESS', payload: info }
+  return { type: 'AUTH::LOGIN_FAILED', payload: null }
 }
--- reducers/auth.reducer.js
+++ reducers/auth.reducer.js
@@ -11,18 +11,18 @@ const initState = {
 
 export default (state = {}, action) => {
   switch (action.type) {
-    case 'AUTH_GET_INFO':
+    case 'AUTH::GET_INFO':
       return update(state, { info: { $set: action.payload } })
-    case 'AUTH_CONTROL_LOGIN_MODAL':
+    case 'AUTH::CONTROL_LOGIN_MODAL':
       return update(state, { loginModalVisible: { $set: action.payload } })
-    case 'AUTH_LOGIN_SUCCESS':
+    case 'AUTH::LOGIN_SUCCESS':
       Notification.success('Success!')
       return update(state, {
         isAuth: { $set: true },
         loginModalVisible: { $set: false },
         identity: { $set: action.payload.username }
       })
-    case 'AUTH_LOGIN_FAILED':
+    case 'AUTH::LOGIN_FAILED':
       return state
     default:
       return state
